4f248a8db79c0d46e1737eacb9e146b921df535b
woof0302 <woof0302@gmail.com>
2024-12-08 20:34:09 +0900

    Add files via upload

---
+++ 4. standLightDriver/4. standLightDriver/ap/standLight/standLight.h
@@ -0,0 +1,37 @@
+﻿#ifndef STANDLIGHT_H_
+#define STANDLIGHT_H_
+
+#define F_CPU 1600000UL
+#include <avr/io.h>
+#include <util/delay.h> 
+
+#include "../../driver/Button/BUTTON.h"
+#include "../../driver/LED/LED.h"
+
+
+#define  LED_DDR	DDRD
+#define	LED_PORT	PORTD
+#define  BUTTON_DDR DDRA
+#define  BUTTON_PIN PINA
+
+
+enum{LED_OFF,MODE1,MODE2,MODE3,MODE4};  //standLight의 상태
+
+void StandLight_init();
+
+void StandLight_run();
+
+void StandLight_eventCheck();
+
+void StandLight_execute();
+
+void StandLight_allOff();
+void StandLight_MODE1();
+void StandLight_MODE2();
+void StandLight_MODE3();
+void StandLight_MODE4();
+
+
+
+
+#endif /* STANDLIGHT_H_ */
---
+++ 4. standLightDriver/4. standLightDriver/driver/led/led.c
@@ -0,0 +1,21 @@
+﻿#include "LED.h"
+
+void Led_init(volatile uint8_t *DDR)
+{
+	Gpio_initPort(DDR,OUTPUT);
+}
+
+void Led_writeData(volatile uint8_t *PORT,uint8_t data)
+{
+	Gpio_writePort(PORT,data);
+}
+
+void Led_allOff(volatile uint8_t *PORT,uint8_t data)
+{
+	Gpio_writePort(PORT,0x00);
+}
+
+void Led_allOn(volatile uint8_t *PORT,uint8_t data)
+{
+	Gpio_writePort(PORT,0xff);
+}
